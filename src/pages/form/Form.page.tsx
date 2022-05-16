261e81cce1701fb68a2d714149659e26642acd7c
Rav Sispal <23012636+rsispal@users.noreply.github.com>
2022-05-16 19:56:05 +0100

    Add QuestionForm to Form Page

--- src/pages/form/Form.page.tsx
+++ src/pages/form/Form.page.tsx
@@ -1,9 +1,15 @@
-import { Box, Heading } from "@chakra-ui/react";
 import { FC } from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import { QuestionForm } from "../../components/QuestionForm";
 import { FormPageProps } from "./Form.types";
+
+import SeedQuestions from "../../__SEED__/data.json";
+import { Question } from "../../components/QuestionForm/QuestionForm.types";
 
 export const FormPage: FC<FormPageProps> = () => (
   <Box w="100vw" h="100vh">
     <Heading>Form Page</Heading>
+
+    <QuestionForm questions={SeedQuestions as Question[]} />
   </Box>
 );
