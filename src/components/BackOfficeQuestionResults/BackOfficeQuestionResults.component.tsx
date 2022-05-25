9f93659b7493de9cbe00a1a038d1beb399a27ccf
Rav Sispal <>
2022-05-25 17:54:41 +0100

    Improve backoffice answer presentation

--- src/components/BackOfficeQuestionResults/BackOfficeQuestionResults.component.tsx
+++ src/components/BackOfficeQuestionResults/BackOfficeQuestionResults.component.tsx
@@ -4,11 +4,34 @@ import { Text } from "@chakra-ui/react";
 import { QuestionFormUtilities } from "../QuestionForm/rc-field-form/QuestionForm.utilities";
 
 import { BackOfficeQuestionResultsProps } from "./BackOfficeQuestionResults.types";
+import {
+  Question,
+  SupportedFormField,
+} from "../QuestionForm/rc-field-form/QuestionForm.types";
 
 export const BackOfficeQuestionResults: FC<BackOfficeQuestionResultsProps> = ({
   answers = {},
   schema,
 }) => {
+  const getAnswerForQuestion = (
+    question: Question,
+    answers: BackOfficeQuestionResultsProps["answers"] = {}
+  ) => {
+    switch (question.field.type) {
+      case SupportedFormField.RadioGroup: {
+        const option =
+          QuestionFormUtilities.getRadioGroupOptionForQuestionByValue(
+            question,
+            answers[question.field.properties.name] ?? ""
+          );
+
+        return option?.label;
+      }
+      default: {
+        return answers[question.field.properties.name];
+      }
+    }
+  };
   return (
     <>
       {Object.keys(answers).map((name) => {
@@ -19,24 +42,16 @@ export const BackOfficeQuestionResults: FC<BackOfficeQuestionResultsProps> = ({
         if (!question) {
           return null;
         }
-        const option =
-          QuestionFormUtilities.getRadioGroupOptionForQuestionByValue(
-            question,
-            answers[name] || ""
-          );
-
-        if (option) {
-          return (
-            <Text>
-              <Text fontWeight="bold" display="inline">
-                [{question?.field.properties.name}]: {question?.field.prompt}
-              </Text>
-              <Text display="inline" marginLeft={4}>
-                {option.label}
-              </Text>
+        return (
+          <Text>
+            <Text fontWeight="bold" display="inline">
+              [{question?.field.properties.name}]: {question?.field.prompt}
             </Text>
-          );
-        }
+            <Text display="inline" marginLeft={4}>
+              {getAnswerForQuestion(question, answers)}
+            </Text>
+          </Text>
+        );
         return null;
       })}
     </>
