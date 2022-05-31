3852d1641677755f90d6fd59fce82cf7338a1713
Rav Sispal <>
2022-05-31 07:18:09 +0100

    Moved Field wrapper component logic into TextInputWrapper

--- src/components/QuestionForm/rc-field-form/field-wrappers/TextInput/TextInput.wrapper.tsx
+++ src/components/QuestionForm/rc-field-form/field-wrappers/TextInput/TextInput.wrapper.tsx
@@ -1,15 +1,18 @@
+import { Field } from "rc-field-form";
 import { FC } from "react";
 import { TextInput } from "../../../../fields/TextInput";
-import { FormField, TextInputProperties } from "../../QuestionForm.types";
+import { Question, TextInputProperties } from "../../QuestionForm.types";
 
 export const TextInputWrapper: FC<{
-  value: string | undefined;
-  onChange: any;
-  field: FormField;
-}> = ({ value, onChange, field }) => (
-  <TextInput
-    {...(field.properties as TextInputProperties)}
-    value={value}
-    onChange={onChange}
-  />
+  question: Question;
+}> = ({ question }) => (
+  <Field name={question.name}>
+    {({ value, onChange }) => (
+      <TextInput
+        {...(question.properties as TextInputProperties)}
+        value={value}
+        onChange={onChange}
+      />
+    )}
+  </Field>
 );
