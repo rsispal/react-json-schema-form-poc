69b6e7547d8936a59811ae93589d3e9bb97269ae
Rav Sispal <>
2022-05-31 08:05:05 +0100

    Add ButtonGroupWrapper component with support for rendering LinkButton and NextQuestionButton

--- src/components/QuestionForm/rc-field-form/field-wrappers/ButtonGroup/ButtonGroup.wrapper.tsx
+++ src/components/QuestionForm/rc-field-form/field-wrappers/ButtonGroup/ButtonGroup.wrapper.tsx
@@ -1,8 +1,37 @@
 import { FC } from "react";
-import { FormField } from "../../QuestionForm.types";
+import { Flex } from "@chakra-ui/react";
+import {
+  ButtonGroupProperties,
+  Question,
+  SupportedFormField,
+} from "../../QuestionForm.types";
+import { NextQuestionButtonWrapper } from "../NextQuestionButton/NextQuestionButton.wrapper";
+import { LinkButtonWrapper } from "../LinkButton/LinkButton.wrapper";
 
 export const ButtonGroupWrapper: FC<{
-  value: string | undefined;
-  onChange: any;
-  field: FormField;
-}> = () => <></>;
+  question: Question;
+}> = ({ question }) => (
+  <Flex
+    flex={1}
+    width={"100%"}
+    flexDirection="row"
+    alignItems={"space-evenly"}
+    justifyContent={"space-evenly"}
+  >
+    {(question.properties as unknown as ButtonGroupProperties).buttons.map(
+      (btn, i) => {
+        switch (btn.type) {
+          case SupportedFormField.LinkButton: {
+            return <LinkButtonWrapper key={i} question={btn} />;
+          }
+          case SupportedFormField.NextQuestionButton: {
+            return <NextQuestionButtonWrapper key={i} question={btn} />;
+          }
+          default: {
+            return null;
+          }
+        }
+      }
+    )}
+  </Flex>
+);
