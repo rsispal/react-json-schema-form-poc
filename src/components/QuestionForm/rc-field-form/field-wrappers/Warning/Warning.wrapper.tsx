cec67aece9218af2de38caf9cb2bdd4470deb871
Rav Sispal <>
2022-06-06 13:14:29 +0100

    Modification to Warning

---
+++ src/components/QuestionForm/rc-field-form/field-wrappers/Warning/Warning.wrapper.tsx
@@ -0,0 +1,40 @@
+import { Alert } from "@chakra-ui/react";
+import { Field } from "rc-field-form";
+import { FC, useRef } from "react";
+import { Warning } from "../../../../fields/Warning";
+import { Question, WarningProperties } from "../../QuestionForm.types";
+
+export const WarningWrapper: FC<{
+  question: Question;
+  onEndFormClickCallback: () => void;
+}> = ({ question, onEndFormClickCallback }) => {
+  const handleClick = () => {
+    if (checkboxRef.current) {
+      checkboxRef.current.click();
+    }
+  };
+
+  const checkboxRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <Field name={question.name}>
+      {({ value, onChange }) => (
+        <>
+          <Warning
+            onContinueClick={handleClick}
+            onEndFormClick={onEndFormClickCallback}
+            {...(question.properties as WarningProperties)}
+          />
+          <input
+            hidden
+            name={question.name}
+            ref={checkboxRef}
+            type="checkbox"
+            defaultChecked={value}
+            onChange={() => onChange(true)}
+          />
+        </>
+      )}
+    </Field>
+  );
+};
