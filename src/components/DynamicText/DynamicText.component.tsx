49459d91bd3e3ff78d5cc4e5c9185fcb8d641546
Rav Sispal <>
2022-08-18 13:11:44 +0100

    Refactor DynamicText

--- src/components/DynamicText/DynamicText.component.tsx
+++ src/components/DynamicText/DynamicText.component.tsx
@@ -1,39 +1,34 @@
 import { Box, Link, Text } from "@chakra-ui/react";
 import { FC } from "react";
-import { DynamicTextProps, TextItem, URLItem } from "./DynamicText.types";
+import { DynamicTextProps } from "./DynamicText.types";
 
-export const DynamicText: FC<DynamicTextProps> = ({ data, dataTestId }) => {
-  const renderItem = (item: TextItem | URLItem, key: number) => {
-    switch (item.type) {
-      case "text": {
-        return item.value;
-      }
-      case "url": {
-        return (
-          <Link
-            color="blue.500"
-            key={key}
-            as="a"
-            target={item.target}
-            href={item.url}
-            display="inline"
-          >
-            {item.label}
-          </Link>
-        );
-      }
-    }
-  };
-
-  return (
-    <Box data-testid={dataTestId}>
-      {data.map((paragraph, key) => (
-        <Text key={key} marginBottom={2}>
-          {paragraph.content.map((item, key) => renderItem(item, key))}
-        </Text>
-      ))}
-    </Box>
-  );
-};
-
-export default DynamicText;
+export const DynamicText: FC<DynamicTextProps> = ({ data, dataTestId }) => (
+  <Box data-testid={dataTestId}>
+    {data.map((paragraph, key) => (
+      <Text key={key} marginBottom={2}>
+        {paragraph.content.map((item, key) => {
+          switch (item.type) {
+            case "text": {
+              return item.value;
+            }
+            case "url": {
+              return (
+                <Link
+                  style={{ display: "inline", color: "blue" }}
+                  key={key}
+                  target={item.target}
+                  href={item.url}
+                >
+                  {item.label}
+                </Link>
+              );
+            }
+            default: {
+              return null;
+            }
+          }
+        })}
+      </Text>
+    ))}
+  </Box>
+);
--- src/components/DynamicText/DynamicText.spec.tsx
+++ src/components/DynamicText/DynamicText.spec.tsx
@@ -2,49 +2,87 @@ import { DynamicTextParagraph, DynamicTextProps } from "./DynamicText.types";
 import { DynamicText } from "./DynamicText.component";
 import { mountWithProps } from "../cypress-component-wrapper";
 
-const sampleData: DynamicTextParagraph[] = [
-  {
-    type: "paragraph",
-    content: [
-      {
-        type: "text",
-        value: "Paragraph 1. This is some text. ",
-      },
-      {
-        type: "url",
-        url: "https://google.co.uk",
-        label: "This is a URL",
-        target: "_blank",
-      },
-      {
-        type: "text",
-        value: ". This is some more text",
-      },
-    ],
-  },
-  {
-    type: "paragraph",
-    content: [
-      {
-        type: "text",
-        value: "Paragraph 2. This is some text. ",
-      },
-      {
-        type: "url",
-        url: "https://google.co.uk",
-        label: "This is a URL",
-        target: "_blank",
-      },
-      {
-        type: "text",
-        value: ". This is some more text",
-      },
-    ],
-  },
-];
-
 describe("<DynamicText /> Component", () => {
+  it("Should render a single text element based on the data schema", () => {
+    const sampleData: DynamicTextParagraph[] = [
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            value: "Lorem ipsum dolor sit amet.",
+          },
+        ],
+      },
+    ];
+    mountWithProps<DynamicTextProps>(DynamicText, {
+      data: sampleData,
+    });
+
+    cy.get("p").contains("Lorem ipsum dolor sit amet.").should("exist");
+  });
+
+  it("Should render a single link element based on the data schema", () => {
+    const sampleData: DynamicTextParagraph[] = [
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "url",
+            url: "https://google.co.uk",
+            target: "_blank",
+            label: "Lorem ipsum dolor sit amet.",
+          },
+        ],
+      },
+    ];
+    mountWithProps<DynamicTextProps>(DynamicText, {
+      data: sampleData,
+    });
+
+    cy.get("a").contains("Lorem ipsum dolor sit amet.").should("exist");
+  });
   it("Should render the elements based on the data schema", () => {
+    const sampleData: DynamicTextParagraph[] = [
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            value: "Paragraph 1. This is some text. ",
+          },
+          {
+            type: "url",
+            url: "https://google.co.uk",
+            label: "This is a URL",
+            target: "_blank",
+          },
+          {
+            type: "text",
+            value: ". This is some more text",
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+        content: [
+          {
+            type: "text",
+            value: "Paragraph 2. This is some text. ",
+          },
+          {
+            type: "url",
+            url: "https://google.co.uk",
+            label: "This is a URL",
+            target: "_blank",
+          },
+          {
+            type: "text",
+            value: ". This is some more text",
+          },
+        ],
+      },
+    ];
     mountWithProps<DynamicTextProps>(DynamicText, {
       data: sampleData,
     });
